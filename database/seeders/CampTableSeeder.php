f96517e0d29d5fc3e9642a1ee7a843ea35babb7a
Dion <riadidion@gmail.com>
2023-10-21 07:45:05 +0700

    add seeder

---
+++ database/seeders/CampTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Camp;
+use Illuminate\Database\Seeder;
+
+class CampTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $camps = [
+            [
+                'title' => 'Gila Belajar',
+                'slug' => 'gila-belajar',
+                'price' => 280,
+            ],
+            [
+                'title' => 'Baru Mulai',
+                'slug' => 'baru-mulai',
+                'price' => 140,
+            ]
+        ];
+
+        foreach ($camps as $key => $camp) {
+            Camp::create($camp);
+        }
+    }
+}
